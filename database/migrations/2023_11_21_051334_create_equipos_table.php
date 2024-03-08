330a38ac4cfe6a34c2a3902f54a95ce9347c6abc
Argenis Rodriguez <estefreric@gmail.com>
2024-03-07 21:06:20 -0400

    mod bd 2

--- database/migrations/2023_11_21_051334_create_equipos_table.php
+++ database/migrations/2023_11_21_051334_create_equipos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('equipos', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('status');
             $table->string('t_equipo');
             $table->string('modelo');
